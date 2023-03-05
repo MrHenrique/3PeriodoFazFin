9ed51df8fc046befd15795283aa9cf90b3baabfc
JoaoVitor <joao12345vitorps2@gmail.com>
2023-03-05 17:06:31 -0300

    Organizando e colocando datas em despesas
    Adicionado a opção de escolher a data nas despesa de trato, e organizado por ordem de criação do detalhe de despesa.

--- src/Realm/Schemas/Schema.js
+++ src/Realm/Schemas/Schema.js
@@ -70,6 +70,7 @@ export const GastosSchema = {
     qtdAli: "float?",
     valorAli: "float?",
     consumoAli: "float?",
+    createdAt: "date",
     assignee: {
       type: "linkingObjects",
       objectType: "RebanhoSchema",
