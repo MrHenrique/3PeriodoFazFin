086169c40dee3a4de0aa5ae219dff5e0e70aca0e
ezequieltsm <ezequielteixeira2004@gmail.com>
2023-05-15 00:27:37 -0300

    Squashed commit of the following:
    commit c6299177253881dbc418f8c13b2d8728e5adddf2
    Author: Gustavo Silvério Rosa <gustavorosasilverio@gmail.com>
    Date:   Mon May 15 01:20:13 2023 -0300
    
        modificação no visual
    
        + aumento na altura dos cards;
        + padding na imagem;
    
    commit d3af39f4154bb9c58368355a35043b323921ff1d
    Merge: 584f6f1 c5322bb
    Author: Gustavo Silvério Rosa <gustavorosasilverio@gmail.com>
    Date:   Mon May 15 01:02:23 2023 -0300
    
        Merge branch 'Branch-Gustavo'
    
    commit c5322bbd0d520cabf54027c124713fb86ed470e6
    Author: Gustavo Silvério Rosa <gustavorosasilverio@gmail.com>
    Date:   Mon May 15 01:02:06 2023 -0300
    
        Visual Estoque;
    
        + Visual para a tela EstoqueGeral;
        + Atualização em algumas cores;
        + Removido Leite do Drawer;
        + Trocado icon de trigo(wheat-sack.png);
        + Trocado icon de seringa(syringe.png);
        + Trocado SplashScreen;
        + Trocado adaptive-icon;

--- src/pages/Estoque/styles.js
+++ src/pages/Estoque/styles.js
@@ -18,16 +18,22 @@ const styles = StyleSheet.create({
   },
   containerInput: {
     flex: 1,
-    justifyContent: 'center',
-  },
-  containerInputTitulo: {
-    flex: 0.45,
-  },
-  fontTitulo:{
-    ...Fonts.txtXLargeBold,
-    color: Colors.white,
+    justifyContent: "center",
+    marginVertical: verticalScale(5),
   },
   font: {
+    color: Colors.white,
+    ...Fonts.txtLargeBold,
+  },
+  fontsubtitulo: {
+    color: Colors.grey,
+    ...Fonts.txtMediumBold,
+  },
+  fontblk: {
+    color: Colors.black,
+    ...Fonts.txtLargeBold,
+  },
+  fontcontainerlistitem: {
     color: Colors.white,
     ...Fonts.txtLargeBold,
   },
@@ -41,7 +47,7 @@ const styles = StyleSheet.create({
   },
   textInputQtd: {
     backgroundColor: Colors.green,
-    paddingHorizontal: verticalScale(10),
+    paddingHorizontal: verticalScale(45),
     textAlign: "center",
     color: Colors.white,
     ...Fonts.txtLargeBold,
@@ -74,13 +80,115 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
   containerButao: {
-    // backgroundColor: Colors.red,
     flex: 1.5,
     flexDirection: "column",
     justifyContent: "flex-end",
   },
+  containerlist: {
+    flex: 1,
+    flexDirection: "column",
+    flexWrap: "wrap",
+    overflow: "scroll",
+    alignContent: "center",
+    marginHorizontal: scale(7),
+  },
+  modalContainer: {
+    borderColor: Colors.white,
+    borderRadius: 10,
+    borderWidth: 1.25,
+    flex: 1,
+    justifyContent: "center",
+    height: verticalScale(140),
+  },
+  listcontainerstyle: {
+    flex: 1,
+    width: "100%",
+  },
+  listcontainerstyleMargin: {
+    flex: 1,
+    width: "100%",
+    marginBottom: scale(-5),
+  },
+  containerItem: {
+    flex: 1,
+    margin: scale(5),
+    borderRadius: 10,
+    backgroundColor: Colors.darkgreenTransparent,
+  },
+  containerText: {
+    flex: 1,
+    ...Fonts.txtLargeBold,
+    alignContent: "center",
+    alignItems: "center",
+    alignSelf: "center",
+    justifyContent: "center",
+  },
+  font: {
+    ...Fonts.txtLarge,
+    color: Colors.white,
+  },
+  Text: {
+    ...Fonts.txtLargeBold,
+    color: Colors.white,
+  },
+  iconArrow: {
+    alignSelf: "center",
+  },
+  iconArrowvirado: {
+    alignSelf: "center",
+    transform: [{ rotate: "180deg" }],
+  },
+  containerValor: {
+    backgroundColor: Colors.darkgreen,
+    margin: scale(10),
+    borderRadius: 10,
+    flex: 0.5,
+    padding: scale(10),
+  },
+  containerlista: {
+    flex: 4,
+  },
+  relatorioCadastro: {
+    flex: 1,
+    flexDirection: "row-reverse",
+    justifyContent: "space-evenly",
+  },
+  containershouldshow: {
+    flex: 10,
+    margin: scale(5),
+  },
+  botaorelatorioproduto: {
+    backgroundColor: Colors.grey,
+    ...Buttons.btnSmallRounded,
+    width: scale(140),
+    height: "100%",
+    justifyContent: "center",
+  },
+  disabled: {
+    opacity: 1,
+  },
+  ListItem: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    borderBottomColor: Colors.white,
+    borderBottomWidth: 1,
+    borderStyle: "dashed",
+  },
+  containerItems: {
+    backgroundColor: Colors.btndarkgreen,
+    marginHorizontal: scale(5),
+  },
+  fontvalortotal: {
+    paddingTop: scale(5),
+    color: Colors.neongreen,
+    ...Fonts.txtLargeBold,
+    alignSelf: "center",
+  },
+  containerImg: {
+    padding: scale(20),
+  },
 });
-
 // estilo tab
 export const estilo = {
   tabarlabel: {
