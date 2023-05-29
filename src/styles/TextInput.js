9c5ec4231a4c1836314389ba8b813f52a38f546d
Gustavo Silvério Rosa <gustavorosasilverio@gmail.com>
2023-05-29 11:06:32 -0300

    - react native floating input
    muito bugado credo

--- src/styles/TextInput.js
+++ src/styles/TextInput.js
@@ -3,7 +3,7 @@ import { scale, verticalScale } from "react-native-size-matters";
 
 export const TextInputLargeRounded = {
       textAlign: "center",
-      borderRadius: 10,
+      borderRadius: 5,
       width: scale(300),
       height: verticalScale(35),
       marginVertical: verticalScale(5),
