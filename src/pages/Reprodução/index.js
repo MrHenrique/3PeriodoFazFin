2f4a86b7ff92c83ef14d93b8d989d488059d4a58
luis267 <luismiguel260602@gmail.com>
2023-05-29 20:09:08 -0300

    comit pois esqueci

--- src/pages/Reprodução/index.js
+++ src/pages/Reprodução/index.js
@@ -6,69 +6,59 @@ import {
   StyleSheet,
   Text,
   TextInput,
-  DatePickerIOS, Button
+  Button
 } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { scale, verticalScale } from "react-native-size-matters";
-import { AuthContext } from "../../contexts/auth";
 import { Feather } from "@expo/vector-icons";
 import { useLinkBuilder } from "@react-navigation/native";
 import uuid from "react-native-uuid";
 import ReproducaoSchema from "../../Realm/ReproducaoSchema";
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, DatePickerIOS, Button } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
+import DatePicker from 'react-native-date-picker'
 
-const DateForm = () => {
-    const [date1, setDate1] = useState(new Date());
-    const [date2, setDate2] = useState(new Date());
-    const [date3, setDate3] = useState(new Date());
-  
-    const handleDate1Change = (newDate) => {
-      setDate1(newDate);
-    };
-  
-    const handleDate2Change = (newDate) => {
-      setDate2(newDate);
-    };
-  
-    const handleDate3Change = (newDate) => {
-      setDate3(newDate);
-    };
-  
-    const handleSubmit = () => {
-      console.log('Date 1:', date1);
-      console.log('Date 2:', date2);
-      console.log('Date 3:', date3);
-    };
-  
-    return (
-      <View style={styles.container}>
-        <Text style={styles.label}>Date 1:</Text>
-        <DatePickerAndroid date={date1} onDateChange={handleDate1Change} />
-  
-        <Text style={styles.label}>Date 2:</Text>
-        <DatePickerAndroid date={date2} onDateChange={handleDate2Change} />
-  
-        <Text style={styles.label}>Date 3:</Text>
-        <DatePickerAndroid date={date3} onDateChange={handleDate3Change} />
-  
-        <Button title="Submit" onPress={handleSubmit} />
-      </View>
-    );
+const EventRegistration = () => {
+  const [coverageDate, setCoverageDate] = useState(new Date());
+  const [creationDate, setCreationDate] = useState(new Date());
+  const [heatDate, setHeatDate] = useState(new Date());
+
+  const handleCoverageDateChange = (selectedDate) => {
+    setCoverageDate(selectedDate);
   };
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      padding: 20,
-    },
-    label: {
-      fontSize: 18,
-      fontWeight: 'bold',
-      marginBottom: 10,
-    },
-  });
-  
-  export default DateForm;
+
+  const handleCreationDateChange = (selectedDate) => {
+    setCreationDate(selectedDate);
+  };
+
+  const handleHeatDateChange = (selectedDate) => {
+    setHeatDate(selectedDate);
+  };
+
+  const registerEvents = () => {
+    // Implementar a lógica para registrar os eventos aqui
+    console.log('Evento de Cobertura:', coverageDate);
+    console.log('Evento de Criação:', creationDate);
+    console.log('Evento de Cio:', heatDate);
+  };
+
+  return (
+    <View>
+      <Text>Registro de Eventos</Text>
+      <Text>Data de Cobertura:</Text>
+      <DatePicker date={coverageDate} onDateChange={handleCoverageDateChange} />
+
+      <Text>Data de Criação:</Text>
+      <DatePicker date={creationDate} onDateChange={handleCreationDateChange} />
+
+      <Text>Data de Cio:</Text>
+      <DatePicker date={heatDate} onDateChange={handleHeatDateChange} />
+
+      <Button title="Registrar Eventos" onPress={registerEvents} />
+    </View>
+  );
+};
+
+export default EventRegistration;
+
+
