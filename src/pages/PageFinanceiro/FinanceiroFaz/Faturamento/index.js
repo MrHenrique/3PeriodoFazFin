b1491d3228de5328c89cafeb52bb638a0331e9ca
JoaoVitor <joao12345vitorps2@gmail.com>
2023-06-03 13:23:09 -0300

    Page Leite/ financeiro reb/faz / Detalhes
    Adicionado e modificado os detalhes das paginas de leite e dos financeiros da fazenda e reb

--- src/pages/PageFinanceiro/FinanceiroFaz/Faturamento/index.js
+++ src/pages/PageFinanceiro/FinanceiroFaz/Faturamento/index.js
@@ -16,23 +16,105 @@ import Modal from "react-native-modal";
 import { AuthContext } from "../../../../contexts/auth";
 import FiltrosData from "../../../../components/Filtros/FiltrosData";
 function Faturamento() {
-  const { precoCF, listaAli, listaLeite, precoLeite , listaFiltrada} = useContext(AuthContext);
+  const { precoCF, listaAli, listaLeite, precoLeite, listaFiltrada } =
+    useContext(AuthContext);
   const [isModalVisible, setModalVisible] = useState(false);
   const [shouldShow, setShouldShow] = useState(false);
+  const [shouldShowDetalhes, setShouldShowDetalhes] = useState(false);
+  const [selectedItemId, setSelectedItemId] = useState(null);
   function toggleModal() {
     setModalVisible(!isModalVisible);
   }
   const imgbg1 = "../../../../../assets/bg2.jpg";
+
+  const handleItemPress = (itemId) => {
+    if (itemId === selectedItemId) {
+      // Se o itemId for o mesmo do item selecionado atualmente,
+      // feche o modal de detalhes
+      setSelectedItemId(null);
+      setShouldShowDetalhes(false);
+    } else {
+      // Se o itemId for diferente do item selecionado atualmente
+      // atualize o itemId e abra o modal de detalhes
+      setSelectedItemId(itemId);
+      setShouldShowDetalhes(true);
+    }
+  };
+
+  const formatarResultado = (valorRecebido, tipo) => {
+    let formattedResult = "";
+    if (tipo == "preco") {
+      const result = valorRecebido.toFixed(2);
+      formattedResult = `R$ ${result.replace(".", ",")}`;
+    } else if (tipo == "prod") {
+      const result = valorRecebido.toFixed(2);
+      formattedResult = `${result.replace(".", ",")} L`;
+    }
+    return formattedResult;
+  };
+
   const renderItem = ({ item }) => {
+    const result = (item.prodL * item.precoL).toFixed(2);
+    const formattedResult = `R$ ${result.replace(".", ",")}`;
+    const formattedData = `${
+      item.createdAt.getDate().toString().padStart(2, 0) +
+      "/" +
+      (item.createdAt.getMonth() + 1).toString().padStart(2, 0) +
+      "/" +
+      item.createdAt.getFullYear().toString()
+    }`;
+    const isItemSelected = item._id === selectedItemId;
     return (
-      <TouchableOpacity style={styles.listaDet}>
-        <Text style={styles.tituloBotao}>
-          {item.createdAt.getDate().toString().padStart(2, 0)}/
-          {(item.createdAt.getMonth() + 1).toString().padStart(2, 0)}/
-          {item.createdAt.getFullYear().toString()} - R${" "}
-          {(item.prodL * item.precoL).toFixed(2)}
-        </Text>
-      </TouchableOpacity>
+      <>
+        <TouchableOpacity
+          style={styles.listaDet}
+          onPress={() => handleItemPress(item._id)}
+        >
+          <Text style={styles.tituloBotao}>
+            {formattedData} - {formattedResult}
+          </Text>
+        </TouchableOpacity>
+        {shouldShowDetalhes && isItemSelected && (
+          <View style={[styles.containerDetalhes]}>
+            <View>
+              <Text style={styles.tituloDetalhes}>Detalhes</Text>
+            </View>
+            <View style={styles.modalContainerText}>
+              <View style={styles.modalContent}>
+                <Text style={styles.textContent}>Data: </Text>
+                <Text style={styles.textContent}>{formattedData}</Text>
+              </View>
+              <View style={styles.modalContent}>
+                <Text style={styles.textContent}>Horario: </Text>
+                <Text style={styles.textContent}>
+                  {item.createdAt.toLocaleTimeString()}
+                </Text>
+              </View>
+              <View style={styles.modalContent}>
+                <Text style={styles.textContent}>Produção: </Text>
+                <Text style={styles.textContent}>
+                  {formatarResultado(item.prodL, "prod")}
+                </Text>
+              </View>
+              <View style={styles.modalContent}>
+                <Text style={styles.textContent}>Preco: </Text>
+                <Text style={styles.textContent}>
+                  {formatarResultado(item.precoL, "preco")}
+                </Text>
+              </View>
+              <View style={styles.modalContent}>
+                <Text style={styles.textContent}>Valor Total: </Text>
+                <Text style={styles.textContent}>
+                  {formatarResultado(item.precoL * item.prodL, "preco")}
+                </Text>
+              </View>
+              <Text style={styles.textContent}>
+                Descrição: {item.description}
+              </Text>
+            </View>
+          </View>
+        )}
+      </>
     );
   };
   function getReceitas() {
@@ -254,6 +336,31 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: 10,
     marginBottom: 12,
+  },
+  containerDetalhes: {
+    width: scale(300),
+    backgroundColor: "white",
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    alignSelf: "center",
+  },
+  modalContent: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  textContent: {
+    fontSize: 20,
+  },
+  modalContainerText: {
+    width: "100%",
+    padding: 5,
+  },
+  tituloDetalhes: {
+    color: "black",
+    fontSize: verticalScale(20),
+    marginBottom: verticalScale(10),
+    textAlign: "center",
+    fontWeight: "bold",
   },
 });
 export default Faturamento;
