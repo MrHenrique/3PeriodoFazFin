5650a0ab99b75348e70995fde93bff1cc49c27f1
Gustavo Silvério Rosa <gustavorosasilverio@gmail.com>
2023-06-08 17:45:59 -0300

    pag reprodução

--- src/pages/Reproducao/index.js
+++ src/pages/Reproducao/index.js
@@ -1,8 +1,17 @@
 import React, { useState } from "react";
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  ScrollView,
+} from "react-native";
 import DateTimePickerModal from "react-native-modal-datetime-picker";
 import { scale, verticalScale } from "react-native-size-matters";
 import { useNavigation } from "@react-navigation/native";
+import styles from "./styles";
+import { AntDesign } from "@expo/vector-icons";
+
 const Reproducao = () => {
   const navigation = useNavigation();
   const [coverageDate, setCoverageDate] = useState(new Date());
@@ -113,314 +122,123 @@ const Reproducao = () => {
   };
 
   return (
-    <View>
-      <Text>Registro de Eventos</Text>
-      <Text>Data de Cobertura:</Text>
-      <View style={styles.containerinfos}>
-        <Text style={styles.tituloinfo}>{text}</Text>
-        <View>
+    <View style={styles.container}>
+      <View style={styles.containergeral}>
+        <Text style={styles.txtTitulo}>Registro de Eventos</Text>
+        {/* CATEGORIA DE REPRODUÇÃO */}
+        {/* CIO */}
+        <ScrollView style={styles.containerScrollView}>
+          <View style={styles.containerCategoria}>
+            <View style={styles.containerTituloCategoria}>
+              <Text style={styles.txtCobertura}>Cio</Text>
+              <AntDesign name="check" size={scale(25)} color="white" />
+              {/* <AntDesign name="close" size={scale(25)} color="white" /> */}
+            </View>
+            <View>
+              <View style={styles.containerRow}>
+                <Text style={styles.txtInfoCategoria}>Último cio:</Text>
+                <Text style={styles.txtInfoCategoria}>02/02/2000</Text>
+              </View>
+              <View style={styles.containerRow}>
+                <Text style={styles.txtInfoCategoria}>Próximo cio:</Text>
+                <Text style={styles.txtInfoCategoria}>10/02/2000</Text>
+              </View>
+              <TouchableOpacity
+                style={styles.botaoData}
+                onPress={showDatePicker}
+              >
+                <Text style={styles.tituloinfo}>Marcar Cio:</Text>
+              </TouchableOpacity>
+
+              <DateTimePickerModal
+                isVisible={isDatePickerVisible}
+                mode="date"
+                onConfirm={handleDateConfirm}
+                onCancel={hideDatePicker}
+                maximumDate={new Date()}
+              />
+            </View>
+          </View>
+          {/* COBERTURA  */}
+          <View style={styles.containerCategoria}>
+            <View style={styles.containerTituloCategoria}>
+              <Text style={styles.txtCobertura}>Cobertura</Text>
+              <AntDesign name="check" size={scale(25)} color="white" />
+              {/* <AntDesign name="close" size={scale(25)} color="white" /> */}
+            </View>
+            <View>
+              <View style={styles.containerRow}>
+                <Text style={styles.txtInfoCategoria}>Ultima Cobertura:</Text>
+                <Text style={styles.txtInfoCategoria}>02/02/2000</Text>
+              </View>
+              <TouchableOpacity
+                style={styles.botaoData}
+                onPress={showDatePicker}
+              >
+                <Text style={styles.tituloinfo}>Marcar Cobertura:</Text>
+              </TouchableOpacity>
+
+              <DateTimePickerModal
+                isVisible={isDatePickerVisible}
+                mode="date"
+                onConfirm={handleDateConfirm}
+                onCancel={hideDatePicker}
+                maximumDate={new Date()}
+              />
+            </View>
+          </View>
+          <View style={styles.containerCategoria}>
+            <View style={styles.containerTituloCategoria}>
+              <Text style={styles.txtCobertura}>Prenhez</Text>
+              {/* <AntDesign name="check" size={scale(25)} color="white" /> */}
+              <AntDesign name="close" size={scale(25)} color="white" />
+            </View>
+            <View>
+              <View style={styles.containerRow}>
+                <Text style={styles.txtInfoCategoria}>Último parto:</Text>
+                <Text style={styles.txtInfoCategoria}>02/01/2000</Text>
+              </View>
+              <View style={styles.containerRow}>
+                <Text style={styles.txtInfoCategoria}>Previsão de parto:</Text>
+                <Text style={styles.txtInfoCategoria}>02/02/2000</Text>
+              </View>
+              <TouchableOpacity
+                style={styles.botaoData}
+                onPress={showDatePicker}
+              >
+                <Text style={styles.tituloinfo}>Marcar Prenhez:</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.botaoData}
+                onPress={showDatePicker}
+              >
+                <Text style={styles.tituloinfo}>Marcar Parto:</Text>
+              </TouchableOpacity>
+
+              <DateTimePickerModal
+                isVisible={isDatePickerVisible}
+                mode="date"
+                onConfirm={handleDateConfirm}
+                onCancel={hideDatePicker}
+                maximumDate={new Date()}
+              />
+            </View>
+          </View>
+        </ScrollView>
+        <View style={styles.containervoltar}>
+          <TouchableOpacity style={styles.botao} onPress={registerEvents}>
+            <Text style={styles.textovoltar}>Cadastrar</Text>
+          </TouchableOpacity>
           <TouchableOpacity
-            style={{
-              backgroundColor: "grey",
-              borderRadius: 20,
-            }}
-            onPress={showDatePicker}
+            style={styles.botao}
+            onPress={() => navigation.navigate("Home")}
           >
-            <Text style={styles.tituloinfo}>Marcar Cobertura :</Text>
+            <Text style={styles.textovoltar}>Voltar</Text>
           </TouchableOpacity>
         </View>
-
-        <DateTimePickerModal
-          isVisible={isDatePickerVisible}
-          mode="date"
-          onConfirm={handleDateConfirm}
-          onCancel={hideDatePicker}
-          maximumDate={new Date()}
-        />
       </View>
-      <Text>Data de Prenhez:</Text>
-      <View style={styles.containerinfos}>
-        <Text style={styles.tituloinfo}>{newsText}</Text>
-        <View>
-          <TouchableOpacity
-            style={{
-              backgroundColor: "grey",
-              borderRadius: 20,
-            }}
-            onPress={newsShowDatePicker}
-          >
-            <Text style={styles.tituloinfo}>Marcar prenhez :</Text>
-          </TouchableOpacity>
-        </View>
-
-        <DateTimePickerModal
-          isVisible={isDatePickerVisible}
-          mode="date"
-          onConfirm={newsHandleDateConfirm}
-          onCancel={newsHideDatePicker}
-          maximumDate={new Date()}
-        />
-      </View>
-      <Text>Data do cio:</Text>
-      <View style={styles.containerinfos}>
-        <Text style={styles.tituloinfo}>{newText}</Text>
-        <View>
-          <TouchableOpacity
-            style={{
-              backgroundColor: "grey",
-              borderRadius: 20,
-            }}
-            onPress={newShowDatePicker}
-          >
-            <Text style={styles.tituloinfo}>Marcar Cio :</Text>
-          </TouchableOpacity>
-        </View>
-
-        <DateTimePickerModal
-          isVisible={isDatePickerVisible}
-          mode="date"
-          onConfirm={handleNewDateConfirm}
-          onCancel={newHideDatePicker}
-          maximumDate={new Date()}
-        />
-      </View>
-      <TouchableOpacity style={styles.botaopress6} onPress={registerEvents}>
-        <Text style={styles.textovoltar}>Cadastrar</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.botaopress}
-        onPress={() => navigation.navigate("Home")}
-      >
-        <Text style={styles.textovoltar}>Voltar</Text>
-      </TouchableOpacity>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  filtroNome: {
-    backgroundColor: "rgba(15, 109, 0, 0.9)",
-    borderRadius: 20,
-    width: scale(140),
-    height: verticalScale(30),
-    margin: verticalScale(5),
-    alignItems: "center",
-    justifyContent: "center",
-    alignSelf: "center",
-  },
-  search: {
-    backgroundColor: "rgba(15, 109, 0, 0.9)",
-    fontSize: verticalScale(15),
-    color: "white",
-    textAlign: "center",
-    alignSelf: "center",
-    height: verticalScale(40),
-    width: scale(300),
-    margin: verticalScale(20),
-    borderRadius: 20,
-  },
-  modalContainer: {
-    backgroundColor: "rgba(234,242,215,1)",
-    position: "absolute",
-    top: verticalScale(10),
-    alignSelf: "center",
-    height: verticalScale(550),
-    width: scale(330),
-    borderRadius: 20,
-  },
-  cardVacas: {
-    backgroundColor: "rgba(15, 109, 0, 0.9)",
-    width: scale(300),
-    height: verticalScale(40),
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 18,
-    marginVertical: verticalScale(4),
-    position: "relative",
-    alignSelf: "center",
-  },
-  container: {
-    flex: 1,
-    backgroundColor: "#004513",
-  },
-  contvoltar: {
-    position: "absolute",
-    alignItems: "center",
-    justifyContent: "center",
-    alignSelf: "center",
-    color: "rgba(15, 109, 0, 0.9)",
-    top: verticalScale(625),
-  },
-  botaovoltar: {
-    backgroundColor: "rgba(15, 109, 0, 0.9)",
-    width: scale(300),
-    height: verticalScale(40),
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 18,
-    marginVertical: verticalScale(5),
-    position: "relative",
-    alignSelf: "center",
-  },
-  botaovoltar2: {
-    backgroundColor: "rgba(15, 109, 0, 0.9)",
-    width: scale(300),
-    height: verticalScale(40),
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 18,
-    marginVertical: verticalScale(5),
-    position: "relative",
-    alignSelf: "center",
-  },
-  botaovoltar3: {
-    backgroundColor: "rgba(15, 109, 0, 0.9)",
-    width: scale(300),
-    height: verticalScale(40),
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 18,
-    marginVertical: verticalScale(5),
-    position: "relative",
-    alignSelf: "center",
-  },
-  textovoltar: {
-    fontSize: verticalScale(14),
-    fontWeight: "bold",
-    color: "#fff",
-  },
-  tituloinfo: {
-    color: "white",
-    fontSize: verticalScale(20),
-    marginBottom: verticalScale(10),
-    textAlign: "center",
-    fontWeight: "bold",
-  },
-  detalhe: {
-    fontSize: verticalScale(20),
-    color: "black",
-    backgroundColor: "white",
-    borderRadius: verticalScale(5),
-    marginBottom: verticalScale(20),
-    textAlign: "center",
-    width: scale(280),
-  },
-  containerinfos: {
-    marginVertical: verticalScale(5),
-    padding: verticalScale(5),
-    width: scale(320),
-    backgroundColor: "rgba(15, 109, 0, 0.7)",
-    borderRadius: 20,
-    alignSelf: "center",
-  },
-  botaoselecionaranimal: {
-    backgroundColor: "#004513",
-    width: scale(215),
-    height: verticalScale(40),
-    alignItems: "center",
-    justifyContent: "center",
-    alignSelf: "center",
-    borderRadius: 18,
-    color: "white",
-  },
-  selecionaranimal: {
-    color: "white",
-    fontSize: verticalScale(20),
-  },
-  container2: {
-    flex: 1,
-    height: verticalScale(100),
-    backgroundColor: "white",
-  },
-  lista2: {
-    backgroundColor: "rgba(15, 109, 0, 0.9)",
-    width: scale(300),
-    height: verticalScale(40),
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 18,
-    alignSelf: "center",
-    marginVertical: verticalScale(5),
-    color: "white",
-    textAlign: "center",
-    fontSize: verticalScale(20),
-    fontWeight: "bold",
-  },
-  container3: {
-    height: verticalScale(300),
-  },
-  botaopressM: {
-    borderRadius: 20,
-    backgroundColor: "rgba(15, 109, 0, 0.9)",
-    width: scale(300),
-    height: verticalScale(40),
-    alignItems: "center",
-    justifyContent: "center",
-    alignSelf: "center",
-    top: verticalScale(580),
-    position: "absolute",
-  },
-
-  tituloBotao: {
-    fontSize: verticalScale(14),
-    fontWeight: "bold",
-    color: "#fff",
-  },
-  botaopress: {
-    borderRadius: 20,
-    backgroundColor: "rgba(15, 109, 0, 0.9)",
-    width: scale(300),
-    height: verticalScale(40),
-    alignItems: "center",
-    justifyContent: "center",
-    alignSelf: "center",
-    top: verticalScale(525),
-    position: "absolute",
-  },
-
-  botaopress6: {
-    borderRadius: 20,
-    backgroundColor: "rgba(15, 109, 0, 0.9)",
-    width: scale(300),
-    height: verticalScale(40),
-    alignItems: "center",
-    justifyContent: "center",
-    alignSelf: "center",
-    top: verticalScale(475),
-    position: "absolute",
-  },
-  TituloM: {
-    justifyContent: "center",
-    alignSelf: "center",
-    color: "#004513",
-    fontSize: verticalScale(30),
-    fontWeight: "bold",
-  },
-  botaoselecionaranimal: {
-    borderRadius: 20,
-    backgroundColor: "rgba(15, 109, 0, 0.9)",
-    top: verticalScale(425),
-    position: "absolute",
-    width: scale(300),
-    height: verticalScale(40),
-    alignItems: "center",
-    justifyContent: "center",
-    alignSelf: "center",
-  },
-  botaopressdisabled: {
-    borderRadius: 20,
-    backgroundColor: "rgba(15, 109, 0, 0.4)",
-    width: scale(300),
-    height: verticalScale(40),
-    alignItems: "center",
-    justifyContent: "center",
-    alignSelf: "center",
-    top: verticalScale(475),
-    position: "absolute",
-  },
-  dateComponente: {
-    width: 350,
-  },
-});
 
 export default Reproducao;
