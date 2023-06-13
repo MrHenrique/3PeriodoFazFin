db9f48ac93f4d90166f5ab0692d20487f176ac0f
Henrique Oliveira Melo <104600158+MrHenrique@users.noreply.github.com>
2023-06-13 17:03:03 -0300

    ZZ

--- src/pages/Leite/AdicionarLeite/index.js
+++ src/pages/Leite/AdicionarLeite/index.js
@@ -108,7 +108,7 @@ function AdicionarLeite({ navigation }) {
                 precoL: parsedValue,
                 prodL: parsedValueProd,
                 description,
-                createdAt: new Date(),
+                createdAt: new Date(date),
               });
               reb.receitas.push(createdLeiteReb);
 
@@ -121,7 +121,7 @@ function AdicionarLeite({ navigation }) {
                   precoL: parsedValue,
                   prodL: parsedValueProd / nVacas,
                   description,
-                  createdAt: new Date(),
+                  createdAt: new Date(date),
                 });
                 vaca.receitas.push(createdLeite);
               });
