788da8502dbdb363df14f0281662fd0732dad245
JoaoVitor <joao12345vitorps2@gmail.com>
2023-06-13 16:02:53 -0300

    + detalhes no click do grafico

--- src/components/Graficos/Graficodetalhesvacas/index.js
+++ src/components/Graficos/Graficodetalhesvacas/index.js
@@ -8,12 +8,28 @@ import { SafeAreaView } from "react-native-safe-area-context";
 function Graficodetalhesvacas() {
   const { grafVaca, listaReceitasVaca, listaDespesasVaca } =
     useContext(AuthContext);
-  const [modalVisible, setModalVisible] = useState(false);
-  const [modalVisible1, setModalVisible1] = useState(false);
+  const [modalReceitasVisible, setModalReceitasVisible] = useState(false);
+  const [modalDespesasVisible, setModalDespesasVisible] = useState(false);
+  const [modalDetReceitasVisible, setModalDetReceitasVisible] = useState(false);
+  const [modalDetDespesasVisible, setModalDetDespesasVisible] = useState(false);
   const [mesSelecionado, setMesSelecionado] = useState(0);
   const [itemSelecionado, setItemSelecionado] = useState(null);
 
   const resultado = Number(grafVaca);
+  const meses = [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro",
+  ];
   const receitasPorVaca = {
     0: 0, //Janeiro
     1: 0, //Fevereiro
@@ -95,6 +111,12 @@ function Graficodetalhesvacas() {
     return dataCriacao.getMonth() === mesSelecionado;
   });
 
+  //Cria um novo array com as despesas do mes selecionado quando clicado em um ponto do grafico.
+  const despesasDoMesSelecionado = listaDespesasVaca.filter((item) => {
+    const dataCriacao = new Date(item.createdAt);
+    return dataCriacao.getMonth() === mesSelecionado;
+  });
+
   /*//Teste
   const getItemValue = (key) => {
     const item = receitasDoMesSelecionado.find((item) => item._id === key);
@@ -102,11 +124,30 @@ function Graficodetalhesvacas() {
   };*/
 
   //Cria um texto com a Data e o Valor daquela produção, usado na flatList
-  const renderItem = ({ item }) => {
+  const renderItemDespesas = ({ item }) => {
     return (
       <TouchableOpacity
         onPress={() => {
-          setModalVisible1(true);
+          setModalDetDespesasVisible(true);
+          setItemSelecionado(item);
+        }}
+      >
+        <Text style={styles.flatListContent}>
+          {item.createdAt.getDate().toString().padStart(2, 0)}/
+          {(item.createdAt.getMonth() + 1).toString().padStart(2, 0)}/
+          {item.createdAt.getFullYear().toString()} -- R${" "}
+          {(item.qtdProd * item.valorProd).toFixed(2)}
+        </Text>
+      </TouchableOpacity>
+    );
+  };
+
+  //Cria um texto com a Data e o Valor daquela produção, usado na flatList
+  const renderItemReceitas = ({ item }) => {
+    return (
+      <TouchableOpacity
+        onPress={() => {
+          setModalDetReceitasVisible(true);
           setItemSelecionado(item);
         }}
       >
@@ -120,18 +161,54 @@ function Graficodetalhesvacas() {
     );
   };
 
+  const handleDataPointClick = ({ value, index, dataset }) => {
+    setMesSelecionado(index); //passa o indice do ponto clicado para a variavel mesSelecionado
+    console.log(value, index, dataset);
+    if (dataset.datasetsID === "receitas") {
+      setModalReceitasVisible(true);
+    } else if (dataset.datasetsID === "despesas") {
+      setModalDespesasVisible(true);
+    }
+  };
+
+  const formatarResultado = (valorRecebido, tipo) => {
+    let formattedResult = "";
+    if (tipo === "preco") {
+      const result = valorRecebido.toFixed(2);
+      formattedResult = `R$ ${result.replace(".", ",")}`;
+    } else if (tipo == "litro") {
+      const result = valorRecebido.toFixed(2);
+      formattedResult = `${result.replace(".", ",")} L`;
+    } else if (tipo === "peso") {
+      const result = valorRecebido.toFixed(2);
+      formattedResult = `${result.replace(".", ",")} Kg`;
+    } else if (tipo === "data") {
+      let valorAFormatar = new Date(valorRecebido);
+      formattedResult = `${
+        valorAFormatar.getDate().toString().padStart(2, 0) +
+        "/" +
+        (valorAFormatar.getMonth() + 1).toString().padStart(2, 0) +
+        "/" +
+        valorAFormatar.getFullYear().toString()
+      }`;
+    }
+    return formattedResult;
+  };
+
   const data = {
     labels: Object.keys(receitasPorVaca) //pega as chaves do objeto (receitasPorVaca)
       .map((mes) => getNomeDoMes(parseInt(mes))), //retorna o nome do mês referente a chave do (receitasPorVaca),
     datasets: [
       {
+        datasetsID: "receitas",
         data: Object.values(receitasPorVaca),
-        color: (opacity = 1) => `rgba(0, 255, 255, ${opacity})`, // cor da primeira linha 
+        color: (opacity = 1) => `rgba(0, 255, 255, ${opacity})`, // cor da primeira linha
         strokeWidth: 3,
       },
       {
+        datasetsID: "despesas",
         data: Object.values(despesasPorVaca),
-        color: (opacity = 1) => `rgba(255, 0, 0, ${opacity})`, // cor da segunda linha 
+        color: (opacity = 1) => `rgba(255, 0, 0, ${opacity})`, // cor da segunda linha
         strokeWidth: 3,
       },
     ],
@@ -154,43 +231,119 @@ function Graficodetalhesvacas() {
         verticalLabelRotation={-90}
         xLabelsOffset={verticalScale(7)}
         bezier
-        onDataPointClick={({ value, index }) => {
-          setMesSelecionado(index); //passa o indice do ponto clicado para a variavel mesSelecionado
-          setModalVisible(true);
-        }}
+        onDataPointClick={handleDataPointClick}
       />
-      <Modal visible={modalVisible} animationType="slide" transparent={true}>
+      <Modal
+        visible={modalReceitasVisible}
+        animationType="slide"
+        transparent={true}
+      >
         <View style={styles.modalContainer}>
           <View style={styles.modalHeader}>
             <Text style={styles.modalTitle}>
-              Detalhes de {data.labels[mesSelecionado]}
+              Detalhes de {meses[mesSelecionado]}
             </Text>
-            <TouchableOpacity onPress={() => setModalVisible(false)}>
+            <TouchableOpacity onPress={() => setModalReceitasVisible(false)}>
               <Text style={styles.modalCloseButton}>X</Text>
             </TouchableOpacity>
           </View>
           <Text style={styles.modalContent}>
-            Receitas em {data.labels[mesSelecionado]}: R${" "}
+            Receitas em {meses[mesSelecionado]}: R${" "}
             {data.datasets[0].data[mesSelecionado].toFixed(2)}
           </Text>
           <FlatList
             style={styles.flatListContainer}
             data={receitasDoMesSelecionado}
-            renderItem={renderItem}
+            renderItem={renderItemReceitas}
             keyExtractor={(item) => item._id}
           />
         </View>
       </Modal>
 
-      <Modal visible={modalVisible1} animationType="slide" transparent={true}>
+      <Modal
+        visible={modalDespesasVisible}
+        animationType="slide"
+        transparent={true}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalHeader}>
+            <Text style={styles.modalTitle}>
+              Detalhes de {meses[mesSelecionado]}
+            </Text>
+            <TouchableOpacity onPress={() => setModalDespesasVisible(false)}>
+              <Text style={styles.modalCloseButton}>X</Text>
+            </TouchableOpacity>
+          </View>
+          <Text style={styles.modalContent}>
+            Despesas em {meses[mesSelecionado]}: R${" "}
+            {data.datasets[0].data[mesSelecionado].toFixed(2)}
+          </Text>
+          <FlatList
+            style={styles.flatListContainer}
+            data={despesasDoMesSelecionado}
+            renderItem={renderItemDespesas}
+            keyExtractor={(item) => item._id}
+          />
+        </View>
+      </Modal>
+
+      <Modal
+        visible={modalDetReceitasVisible}
+        animationType="slide"
+        transparent={true}
+      >
         <SafeAreaView
           style={{ flex: 1 }}
-          onTouchStart={() => setModalVisible1(false)}
+          onTouchStart={() => setModalDetReceitasVisible(false)}
         >
           <View style={styles.modalContainer1}>
             <View style={styles.modalHeader1}>
               <Text style={styles.modalTitle1}>Detalhes</Text>
-              <TouchableOpacity onPress={() => setModalVisible1(false)}>
+              <TouchableOpacity
+                onPress={() => setModalDetReceitasVisible(false)}
+              >
+                <Text style={styles.modalCloseButton1}>X</Text>
+              </TouchableOpacity>
+            </View>
+            {itemSelecionado && (
+              <>
+                <Text style={styles.modalContent1}>
+                  <Text style={{ fontWeight: "bold" }}>Preço:</Text> R${" "}
+                  {itemSelecionado.precoL.toFixed(2)}
+                </Text>
+                <Text style={styles.modalContent1}>
+                  <Text style={{ fontWeight: "bold" }}>Quantidade:</Text>{" "}
+                  {itemSelecionado.prodL}
+                </Text>
+                <Text style={styles.modalContent1}>
+                  <Text style={{ fontWeight: "bold" }}>Data:</Text>{" "}
+                  {itemSelecionado.createdAt.toLocaleDateString()}
+                </Text>
+                <Text style={styles.modalContent1}>
+                  <Text style={{ fontWeight: "bold" }}>Hora:</Text>{" "}
+                  {itemSelecionado.createdAt.toLocaleTimeString()}
+                </Text>
+              </>
+            )}
+          </View>
+        </SafeAreaView>
+      </Modal>
+
+      <Modal
+        visible={modalDetDespesasVisible}
+        animationType="slide"
+        transparent={true}
+      >
+        <SafeAreaView
+          style={{ flex: 1 }}
+          onTouchStart={() => setModalDetDespesasVisible(false)}
+        >
+          <View style={styles.modalContainer1}>
+            <View style={styles.modalHeader1}>
+              <Text style={styles.modalTitle1}>Detalhes</Text>
+              <TouchableOpacity
+                onPress={() => setModalDetDespesasVisible(false)}
+              >
                 <Text style={styles.modalCloseButton1}>X</Text>
               </TouchableOpacity>
             </View>
